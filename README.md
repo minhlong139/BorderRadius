020fdc2e9d22f631226168bceec9c0fa8351a612
Bui Minh Long <minhlong139@gmail.com>
2009-12-30 12:05:33 +0700

    upgrade to 1.2

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ BorderRadius is a Mootools's Plugin. BorderRadius makes corner more beautiful bu
 
 BorderRadius was tested on: 
 ### Broswer: 
-    IE6+, FF3+, Opera9+, Chrome2+, Safari
+    IE6+, FF3+, Opera9+, Chrome2+, Safari4
 ### Platform: 
     WindowXP+, Fedora7+, MacOS
 	
@@ -16,8 +16,34 @@ BorderRadius was tested on:
 How to use
 ----------
 
-### Javascript
+### Javascript:
+    #JS
 	/* initialize */
 	$('#box').borderRadius({'radius: 10'});
 	/* other way */
 	new BorderRadius($('#box'), {'radius': 10});
+	
+### HTML (property options is optional):
+    #HTML
+    <div id="box" options="{'radiusBottomRight':0, 'radiusTopLeft':0, 'css3': false}">Border Radius</div>
+    
+### Options:
+    * radiusTopLeft - (integer: defaults to 0) Radius of top left border corner.
+    * radiusBottomLeft - (integer: defaults to 0) Radius of bottom left border corner.
+    * radiusTopRight - (integer: defaults to 0) Radius of top right border corner.
+    * radiusBottomRight - (integer: defaults to 0) Radius of bottom right border corner.
+    * radiusTop - (integer: defaults to 0) Radius of top left and top right border corner.
+    * radiusBottom - (integer: defaults to 0) Radius of bottom left and bottom right border corner.
+    * radius - (integer: defaults to 0) Radius of 4 border corners.
+    * parentBgColor - (color: defaults to '#fff') Fix parent element's background color.
+    * borderWidth - (integer: defaults to null) Fix border width.
+    * borderStyle - (string: defaults to null) Fix border style.
+    * borderColor - (color: defaults to null) Fix border color.
+    * css3 - (boolean: defaults to true) By default BorderRadius make radius corners by CSS3 if currently browser supported, else BorderRadius create radius corners by itself. If you want to make consitent, set css3 to false
+    
+What's new?
+----------
+### Version 1.2:
+    - Implement BorderRadius options by html property
+    - Specify radius for each corners: top-left, top-right, bottom-left, bottom-right
+
--- Source/border-radius.js
+++ Source/border-radius.js
@@ -21,13 +21,17 @@ var BorderRadius = new Class({
   Implements: [Options],
 
   options: {
-    parentBgColor: '#fff',
+    radiusTopLeft: 0,
+    radiusTopRight: 0,
     radiusTop: 0,
+    radiusBottomLeft: 0,
+    radiusBottomRight: 0,
     radiusBottom: 0,
     radius: 0,
     borderWidth: null,
     borderStyle: null,
     borderColor: null,
+    parentBgColor: '#fff',
     css3: true,
     wrapperClass: 'border-radius-wrapper',
     key: 'border-radius-lDwEgapDpDOIasdlWERSDFxclvkjs234oiSDFSDFsadg@#$098t356sgdlfgjsDFGS@dsfgkyoire'
@@ -42,6 +46,8 @@ var BorderRadius = new Class({
   },
   
   initOptions: function(options) {
+    var opts = JSON.decode(this.element.getProperty('options'));
+    options = $merge(opts, options)
     this.setOptions(options);
     var parentBgColor = this.element.getParent().getStyle('background-color');
     parentBgColor = parentBgColor=='transparent'?this.options.parentBgColor:parentBgColor;
@@ -51,10 +57,23 @@ var BorderRadius = new Class({
     this.options.borderStyle = this.options.borderStyle=='none'?'solid':this.options.borderStyle;
     this.options.borderColor = this.options.borderColor==null?this.element.getStyle('border-top-color'):this.options.borderColor; 
     
-    if (this.options.radius) this.setOptions({
-      'radiusBottom': this.options.radius,
-      'radiusTop':    this.options.radius
+    if (options.radius) this.setOptions({
+      'radiusBottomLeft': options.radius,
+      'radiusTopLeft':    options.radius,
+      'radiusBottomRight': options.radius,
+      'radiusTopRight':    options.radius
     });
+    if (options.radiusTop) this.setOptions({
+      'radiusTopLeft':    options.radiusTop,
+      'radiusTopRight':    options.radiusTop
+    });
+    if (options.radiusBottom) this.setOptions({
+      'radiusBottomLeft': options.radiusBottom,
+      'radiusBottomRight': options.radiusBottom
+    });
+    ['radiusBottomLeft', 'radiusTopLeft', 'radiusBottomRight', 'radiusTopRight'].each(function(pos) {
+      if (options[pos]) this.options[pos] = options[pos];
+    }.bind(this));
     this.setOptions({'parentBgColor': parentBgColor});
   },
   
@@ -91,7 +110,8 @@ var BorderRadius = new Class({
     this.wrap();
     
     var hasBorder = false;
-    var position = {'top': this.options.radiusTop, 'bottom': this.options.radiusBottom};
+    var position = {'top': {'left': this.options.radiusTopLeft, 'right': this.options.radiusTopRight}, 
+                    'bottom': {'left': this.options.radiusBottomLeft, 'right': this.options.radiusBottomRight}};
     
     if (this.options.borderWidth>0) {
       this.element.setStyle('border', 'none');
@@ -102,8 +122,8 @@ var BorderRadius = new Class({
         'border-color': this.options.borderColor,
         'background-color': this.element.getStyle('background-color')
       }
-      if (this.options.radiusBottom!=0) styles['border-bottom'] = 'none';
-      if (this.options.radiusTop!=0) styles['border-top'] = 'none';  
+      if (this.options.radiusBottomLeft!=0 || this.options.radiusBottomRight!=0) styles['border-bottom'] = 'none';
+      if (this.options.radiusTopLeft!=0 || this.options.radiusTopRight!=0) styles['border-top'] = 'none';  
       this.wrapper.setStyles(styles);    
     }
     
@@ -113,26 +133,45 @@ var BorderRadius = new Class({
       });
       el.inject(this.element, id);
       this.borderRadier[id] = el;
-      for (var i=0;i<position[id];i++){
-        var border = position[id] - Math.round(Math.sqrt(position[id]*position[id]-i*i));
+      for (var i=0;i<Math.max(position[id].left, position[id].right);i++){
+        var roundNumLeft = Math.round(Math.sqrt(Math.max(position[id].left*position[id].left-i*i, 0)));
+        var floorNumLeft = Math.floor(Math.sqrt(Math.max(position[id].left*position[id].left-i*i, 0)));
+        var roundNumRight = Math.round(Math.sqrt(Math.max(position[id].right*position[id].right-i*i, 0)));
+        var floorNumRight = Math.floor(Math.sqrt(Math.max(position[id].right*position[id].right-i*i, 0)));
+
+        var borderLeft = position[id].left==0?0:Math.max(position[id].left - roundNumLeft, 0);
+        var borderRight = position[id].right==0?0:Math.max(position[id].right - roundNumRight, 0);
         var bgCornerStyles = {
           'display': 'block',
           'overflow': 'hidden',
           'height': 1,
+          'position': 'relative',
           'border-color': this.options.parentBgColor,
           'border-style': 'solid',
-          'border-width': '0px ' + border + 'px',
+          'border-left-width': borderLeft,
+          'border-right-width': borderRight,
+          'border-top-width': 0,
+          'border-bottom-width': 0,
           'background-color': this.element.getStyle('background-color')
+        };
+        
+        var opacityStyles = {
+          'height': 1,          
+          'border-color': this.options.parentBgColor,
+          'border-style': this.options.borderStyle,
+          'border-width': '0px 1px'
         };
         var borderStyles = {
           'height': 1,          
           'border-color': this.options.borderColor,
           'border-style': this.options.borderStyle,
           'border-width': '0px ' + this.options.borderWidth+ 'px',
-          'border-top-width': i>=position[id]-this.options.borderWidth?this.options.borderWidth:0
+          'border-top-width': i>=Math.max(position[id].left, position[id].right)-this.options.borderWidth?this.options.borderWidth:0
         };
-        var bgCorner = (new Element('div')).setStyles(bgCornerStyles)
+        var bgCorner = (new Element('div', {'class': i})).setStyles(bgCornerStyles)
                                            .inject(el, id);
+        // TODO: Make corners more smoothly
+        //var opacityBorder = (new Element('div')).setStyles(opacityStyles).setOpacity(0.5).inject(bgCorner);                                 
         if (hasBorder) (new Element('div')).setStyles(borderStyles)
                                            .inject(bgCorner);            
       }            
@@ -152,16 +191,16 @@ var BorderRadius = new Class({
     if (this.checkBorderRadius()) {
       var styles = {
         gecko: {
-          '-moz-border-radius-topleft': this.options.radiusTop,
-          '-moz-border-radius-topright': this.options.radiusTop,
-          '-moz-border-radius-bottomleft': this.options.radiusBottom,
-          '-moz-border-radius-bottomright': this.options.radiusBottom
+          '-moz-border-radius-topleft': this.options.radiusTopLeft,
+          '-moz-border-radius-topright': this.options.radiusTopRight,
+          '-moz-border-radius-bottomleft': this.options.radiusBottomLeft,
+          '-moz-border-radius-bottomright': this.options.radiusBottomRight
         },
         webkit: {
-          '-webkit-border-top-left-radius': this.options.radiusTop,
-          '-webkit-border-top-right-radius': this.options.radiusTop,
-          '-webkit-border-bottom-left-radius': this.options.radiusBottom,
-          '-webkit-border-bottom-right-radius': this.options.radiusBottom
+          '-webkit-border-top-left-radius': this.options.radiusTopLeft,
+          '-webkit-border-top-right-radius': this.options.radiusTopRight,
+          '-webkit-border-bottom-left-radius': this.options.radiusBottomLeft,
+          '-webkit-border-bottom-right-radius': this.options.radiusBottomRight
         }
       }
       if (styles[Browser.Engine.name]) {
@@ -189,9 +228,8 @@ var BorderRadius = new Class({
 });
 
 Element.implement({
-  borderRadius: function (options) {
-    var opts = JSON.decode(this.getProperty('options'));
-    var br = new BorderRadius(this, $merge(opts, options));
+  borderRadius: function (options) {    
+    var br = new BorderRadius(this, options);
     this.store('br', br);
     return br;
   }
